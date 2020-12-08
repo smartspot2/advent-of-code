ba6bc2108700ebbd0eeacb179b7904541f73384d
smartspot2 <smartspot2@gmail.com>
2020-12-08 01:59:20 -0800

    Fix typo, align comments

--- 2020/day8-short.py
+++ 2020/day8-short.py
@@ -1,19 +1,19 @@
 (lambda f,d:print(-f(f,d),max(f(f,d[:i]+['nj'['n'in v]]+d[i+1:])for i,v in enumerate(d)if 1-i%2)))(lambda f,d,i=0,a=0,v=[]:(-(i in v)|(i>=len(d)))*a or f(f,d,i+2*(1,(n:=int(d[i+1])))['j'in d[i]],a+n*('a'in d[i]),v+[i]),open('i').read().split())
 
-(lambda f, d: print(  # prints (part 1, part 2)
-    -f(f, d),  # negative = infinite loop, positive = normal exit
-    max(  # max to get the most positive value
+(lambda f, d: print(                                # prints (part 1, part 2)
+    -f(f, d),                                       # negative = infinite loop, positive = normal exit
+    max(                                            # max to get the most positive value
         f(f, d[:i] + ['nj'['n' in v]] + d[i + 1:])  # swap jmp and nop at index i
-        for i, v in enumerate(d)  # iterate through all possible swaps
-        if 1 - i % 2)))(  # only take indices of instructions
-    lambda f, d, i=0, a=0, v=[]:  # recursive function for execution of program
-    (-(i in v)  # if index visited, this is -1
-     | (i >= len(d)))  # if index out of bounds, this is 1
-    * a  # use previous as the sign of a
-    or f(f, d,  # recursive call for next instruction
-         i + 2 * (1,  # move forward 2, because instructions are of length 2
-                  (n := int(d[i + 1]))  # jump to new index if 'jmp'
-                  )['j' in d[i]],  # decide whether ot jump or move forward
-         a + n * ('a' in d[i]),  # increment acc if 'acc'
-         v + [i]),  # add current index to visited
-    open('i').read().split())  # split on each instruction/argument
+        for i, v in enumerate(d)                    # iterate through all possible swaps
+        if 1 - i % 2)))(                            # only take indices of instructions
+    lambda f, d, i=0, a=0, v=[]:                    # recursive function for execution of program
+    (-(i in v)                                      # if index visited, this is -1
+     | (i >= len(d)))                               # if index out of bounds, this is 1
+    * a                                             # use previous as the sign of a
+    or f(f, d,                                      # recursive call for next instruction
+         i + 2 * (1,                                # move forward 2, because instructions are of length 2
+                  (n := int(d[i + 1]))              # jump to new index if 'jmp'
+                  )['j' in d[i]],                   # decide whether to jump or move forward
+         a + n * ('a' in d[i]),                     # increment acc if 'acc'
+         v + [i]),                                  # add current index to visited
+    open('i').read().split())                       # split on each instruction/argument
