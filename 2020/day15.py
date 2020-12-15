9c2574f7b50547257ca4573d03856627dcf6514a
smartspot2 <smartspot2@gmail.com>
2020-12-14 22:01:48 -0800

    Shorten day 15

--- 2020/day15.py
+++ 2020/day15.py
@@ -14,20 +14,13 @@ data = list(map(int, data.split(',')))
 
 p1, p2 = 0, 0
 
-spoken = set(data[:-1])
 last = {v: i for i, v in enumerate(data, 1)}
-
 prev = data[-1]
 times = 30000000
 for i in range(len(data) + 1, times + 1):
-    if prev in spoken:
-        new = i - 1 - last[prev]
-        last[prev] = i - 1
-        prev = new
-    else:
-        last[prev] = i - 1
-        spoken.add(prev)
-        prev = 0
+    new = i - 1 - last[prev] if prev in last else 0
+    last[prev] = i - 1
+    prev = new
 
     if i == 2020:
         p1 = prev
